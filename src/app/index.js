5aa4443294f07778bfc52772f7de16e84b09e420
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-11 22:56:15 -0400

    started setting up the landing controller

--- src/app/index.js
+++ src/app/index.js
@@ -6,8 +6,16 @@ angular.module('hausbrewer', ['ui.router'])
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+        controller: 'MainCtrl',
+		controllerAs: 'main'
+      })
+	  .state('landing', {
+	  	url: '/landing',
+	  	templateURL: 'app/landing/landing.html',
+		controller: 'LandCtrl',
+		controllerAs: 'land'
+		})
+	  ;//end chained controllers
     $urlRouterProvider.otherwise('/');
   })
-;
+;//end config 
---
+++ src/app/landing/landing.controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('hausbrewer')
+  .controller('MainCtrl', function () {
+  });
