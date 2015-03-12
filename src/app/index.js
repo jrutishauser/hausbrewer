d9c3282f8d28d8ab4da626d823d0b7d32a3cc840
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-12 14:27:50 -0400

    sub ui routes work for recipes and fermentation off landing page

--- src/app/index.js
+++ src/app/index.js
@@ -8,18 +8,24 @@ angular.module('hausbrewer', ['ui.router'])
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl',
 		controllerAs: 'main'
-      })
+      	})
 	  .state('landing', {
 	  	url: '/landing',
 	  	templateUrl: 'app/landing/landing.html',
 		controller: 'LandCtrl',
 		controllerAs: 'land'
 		})
-		.state('landing.recipes', {
-		url: '/recipes',
-		templateUrl: 'app/recipes/recipes.html',
+		.state('landing.recipe', {
+		url: '/recipe',
+		templateUrl: 'app/recipe/recipe.html',
 		controller: 'RecipeCtrl',
 		controllerAs: 'recipe'
+		})
+		.state('landing.ferment', {
+		url: '/ferment',
+		templateUrl: 'app/ferment/ferment.html',
+		controller: 'FermentCtrl',
+		controllerAs: 'ferment'
 		})
 	  ;//end chained controllers
     $urlRouterProvider.otherwise('/');
