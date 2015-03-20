c29ecd62d4d5abde03e0442227af752024078d15
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-19 20:49:20 -0400

    adding step by step brew process, just started laying out the method in the controller, added the functions to increment the step

--- src/app/singlerecipe/singlerecipe.controller.js
+++ src/app/singlerecipe/singlerecipe.controller.js
@@ -1,9 +1,32 @@
+(function(){
 'use strict';
 
 angular.module('hausbrewer')
   .controller('SinglerecipeCtrl', function ($firebaseObject, $stateParams) {
-		this.info = new Firebase('https://hausbrewer.firebaseio.com/recipes/'+ $stateParams.user + '/' + $stateParams.singlerecipe); 
-		this.userInfo = $firebaseObject(this.info); 
-		console.log(this.userInfo.grain1);  
+		var info = new Firebase('https://hausbrewer.firebaseio.com/recipes/' + 
+			$stateParams.user + '/' + $stateParams.singlerecipe); 
+		this.userInfo = $firebaseObject(info); 
+		var step = 1;
+		console.log(step);	
+		this.stepfor = function(){
+			if (step === 10) {
+				return;
+			} else {
+				step++;
+				console.log(step);
+				return step;
+			}	
+		};	
+		this.stepback = function(){
+			if (step === 1) {
+				return; 
+			} else {	
+				step--;
+				console.log(step);
+				return step;
+			}	
+		};
+		console.log(step);
   
   });
+})();
